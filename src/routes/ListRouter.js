8c7f7f99081dae27ba5381098fc8162d92ecee0f
marcoshenriquefc <marcosh2267@gmail.com>
2023-01-12 18:21:09 -0300

    put a authenticarion on router

--- src/routes/ListRouter.js
+++ src/routes/ListRouter.js
@@ -1,13 +1,14 @@
 import express from "express";
 import ListController from "../controllers/listController.js";
+import ValidationAuth from "./validationToken.js";
 
 const router = express.Router()
 
 router
-    .get("/listProduct/seach", ListController.listAllProduct)
-    .post("/listProduct", ListController.createNewList)
-    .post("/listProduct/item", ListController.updateItensList)
-    .put("/listProduct/removeItem", ListController.deleteItemList)
-    .put("/listProduct/updateItem", ListController.updateItemList)
+    .get("/listProduct/seach",      ValidationAuth.checkToken, ListController.listAllProduct)
+    .post("/listProduct",           ValidationAuth.checkToken, ListController.createNewList)
+    .post("/listProduct/item",      ValidationAuth.checkToken, ListController.updateItensList)
+    .put("/listProduct/removeItem", ValidationAuth.checkToken, ListController.deleteItemList)
+    .put("/listProduct/updateItem", ValidationAuth.checkToken, ListController.updateItemList)
 
 export default router
