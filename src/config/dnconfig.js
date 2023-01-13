cf032055c2dcbf13e02622edf7e698491c033e4e
marcoshenriquefc <83990032+marcoshenriquefc@users.noreply.github.com>
2023-01-13 17:22:46 -0300

    test

--- src/config/dnconfig.js
+++ src/config/dnconfig.js
@@ -8,7 +8,7 @@ const passDB = process.env.DB_PASS;
 
 console.log(userDB);
 
-mongoose.connect(`mongodb+srv://${userDB}:${passDB}@smartmarket.ymstrgv.mongodb.net/smartMarket-data`);
+mongoose.connect(`mongodb+srv://smartMarketApi02267CI:WdUOX3NckoFRQYFF@smartmarket.ymstrgv.mongodb.net/smartMarket-data`);
 
 const db = mongoose.connection;
 
